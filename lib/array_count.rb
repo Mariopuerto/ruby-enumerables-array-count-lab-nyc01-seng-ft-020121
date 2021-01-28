31cb54335539aa41b4e8fb713288ff6552cb0e21
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 00:37:12 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,6 @@
 def count_strings(array)
-array.count { |strings| array > 0 }
+array.count { |element| element.class == string}
+
  # Return the total number of strings in the provided array using the count enumerable
 end
 
